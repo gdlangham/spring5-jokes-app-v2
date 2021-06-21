6a69c74bec87dcc157dbeb0faa3d61add0ba8e73
Greg Langham <gdlangham@gmail.com>
2021-06-21 14:05:02 -0500

    Switched controller to use Interface and changed mapping to support empty path

--- src/main/java/org/springframeworkguru/spring5jokesappv2/controller/JokesController.java
+++ src/main/java/org/springframeworkguru/spring5jokesappv2/controller/JokesController.java
@@ -3,17 +3,18 @@ package org.springframeworkguru.spring5jokesappv2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframeworkguru.spring5jokesappv2.service.JokeService;
 import org.springframeworkguru.spring5jokesappv2.service.JokeServiceImpl;
 
 @Controller
 public class JokesController {
-    private final JokeServiceImpl jokeService;
+    private final JokeService jokeService;
 
-    public JokesController(JokeServiceImpl jokeService) {
+    public JokesController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping("/")
+    @RequestMapping({"/",""})
     public String getJokes(Model model) {
         model.addAttribute("joke",jokeService.getRandomJoke());
         return "index";
